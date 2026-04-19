0156e2eefa33aa5956a80ca04d367ecf698e40e9
akaashbella <akaash.bella@gmail.com>
2026-04-19 16:17:41 -0700

    Add global app state variables and menu DOM references in main.js to support menu navigation and dialog handling

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,25 @@
  * Definitions of variables to maintain app state and provide
  * convenient access to frequently used DOM elements.
  *************************************************************************/
+let GlobalHistoryLogging = true;
+let GlobalDialogClose = null;
 
 /************************************/
 /* MENU VARIABLES                   */
 /************************************/
+const GlobalMenuBtn =  document.getElementById("menuBtn"); 
+const GlobalMenuItems = document.querySelectorAll("li[role='menuitem']");
+
+//Note: Per Josh Wulf's blog post, we implement all immutable global variables using
+//immediately invoked function expressions
+const GlobalFocusedMenuItem = (() => {
+    let _focusedMenuItem = 0
+    const Store = {
+        get: () => _focusedMenuItem,
+        set: val => (_focusedMenuItem = val)
+    }
+    return Object.freeze(Store)
+})()
 
 /************************************/
 /* MODE TAB VARIABLES               */
