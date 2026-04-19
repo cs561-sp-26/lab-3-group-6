7c309066c4154912ccc5e85ceb2fb9a51e2edc31
akaashbella <akaash.bella@gmail.com>
2026-04-19 16:19:46 -0700

    Add mode-to-route mapping constants in main.js to support tab navigation and routing

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,25 @@ const GlobalFocusedMenuItem = (() => {
 /************************************/
 /* MODE TAB VARIABLES               */
 /************************************/
+//Array of mode names
+const GlobalModeNumbersToModes = new Map([
+    [0, "ACTIVITY_FEED"],
+    [1, "ROUNDS"],
+    [2, "COURSES"],
+    [3, "BUDDIES"]
+  ]);
+  const GlobalModeNumbersToRoutes = new Map([
+    [0, "/activityfeed"],
+    [1, "/rounds"],
+    [2, "/courses"],
+    [3, "/buddies"]
+  ]);
+  const GlobalModeDialogNumbersToRoutes = new Map([
+    [0, "/activityfeed/newpost"],
+    [1, "/rounds/newround"],
+    [2, "/courses/addcourse"],
+    [3, "/buddies/findbuddy"]
+  ]);
 
 /*****************************************************/
 /* FLOATING ACTION BUTTON AND MODAL DIALOG VARIABLES */
