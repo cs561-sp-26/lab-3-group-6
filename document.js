584047c257d24f2d28f1f26a4f27f624a1f3cc76
Roshan Aditya Murali <dancingrosievalo@gmail.com>
2026-04-19 17:28:16 -0700

    Route keydown events to menu button and menu item handlers

--- document.js
+++ document.js
@@ -3,6 +3,17 @@
  * These functions support interaction with the top-level document
  * element.
  ************************************************************************/
-
-document.addEventListener("keydown", function(e) { 
+/*************************************************************************
+ * @function Document Keydown Event Handler 
+ * @desc 
+ * When the user presses a key in the app, we interpret the
+ * keypress based on which user interface element currently has focus. 
+ *************************************************************************/
+document.addEventListener("keydown", function(e) {
+    if (document.activeElement.id === "menuBtn") {
+        keyDownMenuBtnFocused(e.code); 
+    } else if (document.activeElement.getAttribute("role") 
+               === "menuitem") {
+        keyDownMenuItemFocused(e.code);
+    }
 });
